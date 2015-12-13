883227c8853f17767cee2a11b89e7ee28dfe4869
Piere Wöhl <piere@pierewoehl.de>
2015-12-14 00:28:14 +0100

    Setup Dev Environment

---
+++ include/classes/DNSConnector.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of DNSConnector
+ *
+ * @author Piere
+ */
+class DNSConnector {
+    //put your code here
+}
---
+++ include/classes/ViewController.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ViewController
+ *
+ * @author Piere
+ */
+class ViewController {
+    //put your code here
+}
